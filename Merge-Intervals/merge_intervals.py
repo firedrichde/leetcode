a2d4f3368233796022c68dec691604ad2dc17abd
nuaawzj <30956549+nuaawzj@users.noreply.github.com>
2020-12-10 15:01:48 +0800

    I have solved problem 'Merge Intervals'

--- Merge-Intervals/merge_intervals.py
+++ Merge-Intervals/merge_intervals.py
@@ -1,3 +1,6 @@
+import functools
+
+
 class Solution(object):
     class Interval(object):
         def __init__(self, item: list):
@@ -23,10 +26,21 @@ class Solution(object):
                 if self.end < interval.start:
                     return None
                 else:
-                    return Solution.Interval([self.start, max(self.end, interval.end)])
+                    return Solution.Interval(
+                        [self.start, max(self.end, interval.end)])
 
         def toList(self):
             return [self.start, self.end]
+
+        @staticmethod
+        def compare(x, y) -> int:
+            """
+            docstring
+            """
+            if x.start == y.start:
+                return x.end - y.end
+            else:
+                return x.start-y.end
 
     def merge(self, intervals: list) -> list:
         """
@@ -34,7 +48,7 @@ class Solution(object):
         """
         intervals = [self.Interval(item) for item in intervals]
         if not self.isSorted(intervals):
-            self.sort(intervals)
+            intervals = self.sort(intervals)
         new_intervals = self.merge0(
             intervals, 0, len(intervals)-1)
         result = []
@@ -43,8 +57,8 @@ class Solution(object):
         return result
 
     def merge0(self, intervals: list, low: int, high: int) -> list:
+        ret = list()
         if low == high:
-            ret = list()
             ret.append(self.Interval(
                 [intervals[low].start, intervals[low].end]))
             return ret
@@ -52,39 +66,20 @@ class Solution(object):
             mid = (low+high)//2
             left = self.merge0(intervals, low, mid)
             right = self.merge0(intervals, mid+1, high)
-            overlap_ret = left[-1].overlap(right[0])
-            ret = list()
-            if overlap_ret == None:
-                for elem in left:
-                    ret.append(elem)
-                for elem in right:
-                    ret.append(elem)
-                return ret
-            else:
-                left_index = len(left)-1
-                tmp = None
-                while left_index >= 0:
-                    tmp = overlap_ret
-                    overlap_ret = overlap_ret.overlap(left[left_index])
-                    if overlap_ret is None:
-                        break
-                    left_index -= 1
-                if overlap_ret is None:
-                    ret.extend(left[0:left_index+1])
-                    overlap_ret = tmp
-                right_index = 0
-                while right_index < len(right):
-                    tmp = overlap_ret
-                    overlap_ret = overlap_ret.overlap(right[right_index])
-                    if overlap_ret is None:
-                        break
-                    right_index += 1
-                if overlap_ret is None:
-                    ret.append(tmp)
-                    ret.extend(right[right_index:])
+            for i in range(len(left)-1):
+                ret.append(left[i])
+            current_interval = left[-1]
+            index = 0
+            while index <= len(right)-1:
+                tmp = current_interval.overlap(right[index])
+                if tmp is None:
+                    ret.append(current_interval)
+                    current_interval = right[index]
                 else:
-                    ret.append(overlap_ret)
-                return ret
+                    current_interval = tmp
+                index += 1
+            ret.append(current_interval)
+            return ret
 
     def isSorted(self, intervals: list) -> list:
         intervals_len = len(intervals)
@@ -97,19 +92,24 @@ class Solution(object):
             return True
 
     def sort(self, intervals: list):
-        intervals_len = len(intervals)
-        for i in range(intervals_len):
-            min_index = i
-            for j in range(i+1, intervals_len):
-                if intervals[j].compareTo(intervals[min_index]) < 0:
-                    min_index = j
-            tmp = intervals[min_index]
-            intervals[min_index] = intervals[i]
-            intervals[i] = tmp
+        # print(intervals)
+        return sorted(intervals, key=functools.cmp_to_key(self.Interval.compareTo))
+        # intervals_len = len(intervals)
+        # for i in range(intervals_len):
+        #     min_index = i
+        #     for j in range(i+1, intervals_len):
+        #         if intervals[j].compareTo(intervals[min_index]) < 0:
+        #             min_index = j
+        #     tmp = intervals[min_index]
+        #     intervals[min_index] = intervals[i]
+        #     intervals[i] = tmp
+        # print(intervals)
 
 
 if __name__ == "__main__":
     test_sol = Solution()
-    # intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
-    intervals = [[1, 4], [4, 5]]
+    # intervals = [[1, 3], [2, 6], [8, 10], [15, 18],[1,20]]
+    intervals = [[4, 5], [2, 4], [4, 6], [
+        3, 4], [0, 0], [1, 1], [3, 5], [2, 2]]
+    # intervals = [[1, 4], [4, 5]]
     print(test_sol.merge(intervals))
