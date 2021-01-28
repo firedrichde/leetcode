3faf7dafc6c5560d9466738249e90999c69b0108
nuaawzj <30956549+nuaawzj@users.noreply.github.com>
2021-01-28 21:33:50 +0800

    solve the problem 'Word Break'

--- Stock-Buy-Sell/stock_buy_sellIII.py
+++ Stock-Buy-Sell/stock_buy_sellIII.py
@@ -2,39 +2,19 @@ import builtins
 
 
 class Solution:
-    def __init__(self) -> None:
-        self.valuesaved = dict()
 
     def maxProfit(self, prices: list) -> int:
-        prices_len = len(prices)
-        profit = 0
-        if prices_len < 2:
-            return profit
-        else:
-            profit = self.maxProfit0(prices, 0, 4, False)
-            return profit
-
-    def maxProfit0(self, prices: list, index: int, count: int, canSell: bool) -> int:
-        if index >= len(prices) or count == 0:
-            return 0
-        else:
-            if (index, count, canSell) in self.valuesaved:
-                return self.valuesaved[(index, count, canSell)]
-            else:
-                # while index < len(prices)-1:
-                #     if canSell and prices[index+1] > prices[index]:
-                #         index += 1
-                #     elif not canSell and prices[index+1] < prices[index]:
-                #         index += 1
-                #     else:
-                #         break
-                transtraction = self.maxProfit0(
-                    prices, index, count-1, not canSell) + (prices[index] if canSell else -prices[index])
-
-                skip = self.maxProfit0(prices, index+1, count, canSell)
-                self.valuesaved[(index, count, canSell)] = max(
-                    transtraction, skip)
-                return self.valuesaved[(index, count, canSell)]
+        min_value = -pow(10, 5)
+        profit_index_transaction2_unhold = 0
+        profit_index_transaction2_hold = min_value
+        profit_index_transaction1_unhold = 0
+        profit_index_transaction1_hold = min_value
+        for price in prices:
+            profit_index_transaction2_unhold = max(profit_index_transaction2_unhold,profit_index_transaction2_hold+price)
+            profit_index_transaction2_hold = max(profit_index_transaction2_hold,profit_index_transaction1_unhold-price)
+            profit_index_transaction1_unhold = max(profit_index_transaction1_unhold, profit_index_transaction1_hold+price)
+            profit_index_transaction1_hold = max(profit_index_transaction1_hold, -price)
+        return profit_index_transaction2_unhold
 
 
 if __name__ == "__main__":
